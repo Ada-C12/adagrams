a51d16961423774271cee4b1eb767fa02ac0ef46
dora1405 <li.dora05@gmail.com>
2019-08-12 15:55:07 -0700

    Figured out pool of letters loop

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,12 +1,27 @@
 
-letters = ["b", "b", "p", "p", "g", "g", "g", "d", "d", "d", "d"].shuffle
-# get 10 strings
-generate = []
-10.times do
-  generate << letters.last
-  letters.pop
+# letters = ["b", "b", "p", "p", "g", "g", "g", "d", "d", "d", "d"].shuffle
+# # get 10 strings
+# generate = []
+# 10.times do
+#   generate << letters.last
+#   letters.pop
+# end
+# puts generate
+
+
+# draw_letters
+
+letter_v2 = {"9" => ["A", "I"], "4" => ["D", "L", "S", "U"]}
+total_letters = []
+
+letter_v2.each do |key, value|
+  key_i = key.to_i
+  key_i.times do
+    value.each do |string|
+      total_letters << string
+    end
+  end
+  # end
 end
-puts generate
 
-
-draw_letters
+print total_letters
