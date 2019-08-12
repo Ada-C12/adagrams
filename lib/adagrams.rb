f269e3ba61b4b6a48830cada0ced3fc16a79709e
dora1405 <li.dora05@gmail.com>
2019-08-12 16:04:33 -0700

    Method defined

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,27 +1,24 @@
-
-# letters = ["b", "b", "p", "p", "g", "g", "g", "d", "d", "d", "d"].shuffle
-# # get 10 strings
-# generate = []
-# 10.times do
-#   generate << letters.last
-#   letters.pop
-# end
-# puts generate
-
-
-# draw_letters
-
-letter_v2 = {"9" => ["A", "I"], "4" => ["D", "L", "S", "U"]}
-total_letters = []
-
-letter_v2.each do |key, value|
-  key_i = key.to_i
-  key_i.times do
-    value.each do |string|
-      total_letters << string
+def draw_letters(letter_pool)
+  letters = []
+  
+  letter_pool.each do |key, value|
+    key_i = key.to_i
+    key_i.times do
+      value.each do |string|
+        letters << string
+      end
     end
   end
-  # end
+  
+  generate = []
+  10.times do
+    generate << letters.last
+    letters.pop
+  end
+  return generate
+  
 end
 
-print total_letters
+
+letter_v2 = {"9" => ["A", "I"], "4" => ["D", "L", "S", "U"]}
+print draw_letters(letter_v2)
