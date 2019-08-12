c9b84cfed5620a7b9d238c3b427907e053f8d01c
Caroline <CarolineWuKaplan@gmail.com>
2019-08-12 16:23:11 -0700

    wave 3 successful

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -63,3 +63,25 @@ end
 
 
 ####################### WAVE 3 ##########################
+
+def score_word(word)
+  score_hash = { A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1, 
+    D: 2, G: 2, B: 3, C: 3, M: 3, P: 3, F: 4, H: 4, V: 4, W: 4, Y: 4, 
+    K: 5, J: 8, X: 8, Q: 10, Z: 10 }
+  
+  word_array = word.upcase.split(//)
+  score = 0
+  word_array.each do |letter|
+    score += score_hash[letter.to_sym] 
+  end
+
+  if word_array.length >= 7 && word_array.length < 11
+    score += 8
+  end
+
+  return score
+end
+
+# p score_word("hahahahaha")
+
+
