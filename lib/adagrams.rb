cee4e7c54a5740177174eb587d8192065c5764f7
Julia Bouvier <juliabouv@gmail.com>
2019-08-12 16:07:41 -0700

    implemented wave 1--draw_letters

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,29 +1,42 @@
-letters = {
-  A: 9,
-  B: 2,
-  C: 2,
-  D: 4,
-  E: 12,
-  F: 2,
-  G: 3,
-  H: 2,
-  I: 9,
-  J: 1,
-  K: 1,
-  L: 4,
-  M: 2,
-  N: 6,
-  O: 8,
-  P: 2,
-  Q: 1,
-  R: 6,
-  S: 4,
-  T: 6,
-  U: 4,
-  V: 2,
-  W: 2,
-  X: 1,
-  Y: 2,
-  Z: 1
-}
+def draw_letters()
+  distribution = {
+    A: ["A", "A", "A", "A", "A", "A", "A", "A", "A"],
+    B: ["B", "B"],
+    C: ["C", "C"],
+    D: ["D", "D", "D", "D"],
+    E: ["E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"],
+    F: ["F", "F"],
+    G: ["G", "G", "G"],
+    H: ["H", "H"],
+    I: ["I", "I", "I", "I", "I", "I", "I", "I", "I"],
+    J: ["J"],
+    K: ["K"],
+    L: ["L", "L", "L", "L"],
+    M: ["M", "M"],
+    N: ["N", "N", "N", "N", "N", "N"],
+    O: ["O", "O", "O", "O", "O", "O", "O", "O"],
+    P: ["P", "P"],
+    Q: ["Q"],
+    R: ["R", "R", "R", "R", "R", "R"],
+    S: ["S", "S", "S", "S"],
+    T: ["T", "T", "T", "T", "T", "T"],
+    U: ["U", "U", "U", "U"],
+    V: ["V", "V"],
+    W: ["W", "W"],
+    X: ["X"],
+    Y: ["Y", "Y"],
+    Z: ["Z"]
+  }
+  
+  letters = distribution.values.flatten
+  # array of 10 strings
+  strings = []
+  
+  10.times do |index| 
+    strings << letters.sample
+    letters.shuffle!
+  end
+  
+  return strings
+end
 
