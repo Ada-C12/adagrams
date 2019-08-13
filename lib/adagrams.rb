f0eb78874648fd207ff1e453c74ad7507b00da1f
Julia Kingrey <juliakingrey@Julias-MBP.localdomain>
2019-08-13 16:29:43 -0700

    added code to compare letters in hand to letters in word

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,32 +1,5 @@
-
+# require pry
 # letter_pool = %w(a a a a a a a a a b b c c d d d d e e e e e e e e e e e e f f g g g h h i i i i i i i i i j k l l l l m m n n n n n n o o o o o o o o p p q r r r r r r s s s s t t t t t t u u u u v v w w x y y z)
-
-# remaining_pool = %w(a a a a a a a a a b b c c d d d d e e e e e e e e e e e e f f g g g h h i i i i i i i i i j k l l l l m m n n n n n n o o o o o o o o p p q r r r r r r s s s s t t t t t t u u u u v v w w x y y z)
-
-# # user_hand = letter_pool.sample(10)
-
-# # p user_hand
-
-# user_hand = []
-# # randomize remaining_pool and assign randomized pool to new variable shuffled_remaining_pool
-# # shuffled_remaining_pool = remaining_pool.shuffle
-# # p shuffled_remaining_pool
-
-# 10.times do |letter|
-#   letter = remaining_pool.sample
-#   user_hand.push(letter)
-#   remaining_pool.slice!(remaining_pool.index(letter))
-# end 
-# p user_hand
-# p remaining_pool
-
-
-# Note from Julia: per the specs, the user hand needs to have capital letters
-# Sample code using map! to upcase
-# array = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-# array.map!(&:upcase)
-# array
-# => ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"]
 
 # def draw_letters
 #   remaining_pool = %w(a a a a a a a a a b b c c d d d d e e e e e e e e e e e e f f g g g h h i i i i i i i i i j k l l l l m m n n n n n n o o o o o o o o p p q r r r r r r s s s s t t t t t t u u u u v v w w x y y z)
@@ -40,16 +13,51 @@
 #   end 
 #   p remaining_pool
 #   return letters_in_hand
-
 # end 
 
 # result = draw_letters
 # p result 
 
 # def uses_available_letters(input, letters_in_hand)
+#   print "Please enter a word: "
+#   input = "Hello"
+#   binding.pry
 #   input_array = input.chars
-#   p input_array
+
+#   input_array.each do |letter|
+#     if letters_in_hand.include?(letter) 
+#       match = "true"
+#     else 
+#       match = "false"
+#     end
+#   end 
+#   return match
 # end 
-input = 'Hello'
+
+# test = uses_available_letters(input, letters_in_hand)
+# puts test
+
+remaining_pool = %w(a a a a a a a a a b b c c d d d d e e e e e e e e e e e e f f g g g h h i i i i i i i i i j k l l l l m m n n n n n n o o o o o o o o p p q r r r r r r s s s s t t t t t t u u u u v v w w x y y z)
+remaining_pool.map!(&:upcase)
+letters_in_hand = []
+
+10.times do |letter|
+  letter = remaining_pool.sample
+  letters_in_hand.push(letter)
+  remaining_pool.slice!(remaining_pool.index(letter))
+end 
+
+p letters_in_hand
+
+
+print "Please enter a word: "
+input = gets.chomp.upcase
 input_array = input.chars
-p input_array
+
+input_array.each do |letter|
+  if letters_in_hand.include?(letter) 
+    puts true
+  else 
+    puts false
+  end
+end 
