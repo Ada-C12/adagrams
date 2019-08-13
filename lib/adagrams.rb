d4208036ca1149926b2452bb1631b9d0f1dd1440
Hallie <idhallie@gmail.com>
2019-08-13 16:01:58 -0700

    Fixed bug with double letter entry.

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,46 +1,47 @@
+
 # Wave 1
 
 def draw_letters
   quantity_by_letter = {
-    A: 9,
-    B: 2,
-    C: 2,
-    D: 4,
-    E: 12,
-    F: 2,
-    G: 3,
-    H: 2,
-    I: 9,
-    J: 1,
-    K: 1,
-    L: 4,
-    M: 2,
-    N: 6,
-    O: 8,
-    P: 2,
-    Q: 1,
-    R: 6,
-    S: 4,
-    T: 6,
-    U: 4,
-    V: 2,
-    W: 2,
-    X: 1,
-    Y: 2,
-    Z: 1
-  }
-  
-  array_letters = quantity_by_letter.map do |letter, quantity|
-    letter.to_s * quantity
-  end
-  
-  array_letters = array_letters.join("")
-  array_letters = array_letters.split('')
-  
-  array_hand = array_letters.sample(10)
-  
-  return array_hand
-  
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1
+}
+
+array_letters = quantity_by_letter.map do |letter, quantity|
+  letter.to_s * quantity
+end
+
+array_letters = array_letters.join("")
+array_letters = array_letters.split('')
+
+array_hand = array_letters.sample(10)
+
+return array_hand
+
 end
 
 # Wave 2
@@ -52,11 +53,12 @@ def uses_available_letters?(user_input_word, letters_in_hand)
   
   user_input_array.each do |character|
     if letters_in_hand.include?(character)
-      letters_in_hand.delete(character)
+      letters_in_hand.delete_at(letters_in_hand.index(character))
     elsif
       validator += 1
       break
     end 
+    
   end
   
   return validator == 0
@@ -64,7 +66,8 @@ def uses_available_letters?(user_input_word, letters_in_hand)
 end
 
 # Wave 3
-values_by_letter = {
+def score_word(word)
+  values_by_letter = {
   A: 1,
   B: 3,
   C: 3,
@@ -82,13 +85,31 @@ values_by_letter = {
   O: 1,
   P: 3,
   Q: 10,
-  R: 6,
-  S: 4,
-  T: 6,
-  U: 4,
-  V: 2,
-  W: 2,
-  X: 1,
-  Y: 2,
-  Z: 1
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10
 }
+
+word = word.upcase.split("")
+
+total = 0
+
+word.each do |character|
+  total += values_by_letter[character.to_sym]
+end
+
+if word.length >= 7
+  total += 8
+else
+  total
+end
+
+return total
+
+end
