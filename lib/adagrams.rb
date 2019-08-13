1296b2503c5905ce9711087c3e53b36f7e605404
dora1405 <li.dora05@gmail.com>
2019-08-13 16:11:09 -0700

    Need to fix Wave 2 delete method, else working

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,47 +1,51 @@
 # Wave 1
 def draw_letters
   letter_pool = {
-  "9" => ["A", "I"], 
-  "4" => ["D", "L", "S", "U"], 
-  "2" => ["B", "C", "F", "H", "M", "P", "V", "W", "Y"],  
-  "12" => ["E"],  
-  "3" => ["G",],
-  "1" => ["J", "K", "Q", "X", "Z"],
-  "6" => ["N", "R", "T"],
-  "8" => ["O"],
-}
-
-letters = []
-
-letter_pool.each do |key, value|
-  key_i = key.to_i
-  key_i.times do
-    value.each do |string|
-      letters << string
+    "9" => ["A", "I"], 
+    "4" => ["D", "L", "S", "U"], 
+    "2" => ["B", "C", "F", "H", "M", "P", "V", "W", "Y"],  
+    "12" => ["E"],  
+    "3" => ["G",],
+    "1" => ["J", "K", "Q", "X", "Z"],
+    "6" => ["N", "R", "T"],
+    "8" => ["O"],
+  }
+  
+  letters = []
+  
+  letter_pool.each do |key, value|
+    key_i = key.to_i
+    key_i.times do
+      value.each do |string|
+        letters << string
+      end
     end
   end
-end
-
-generate = []
-10.times do
-  generate << letters.shuffle.last
-  letters.pop
-end
-return generate
+  
+  generate = []
+  10.times do
+    generate << letters.shuffle.last
+    letters.pop
+  end
+  return generate
 end
 
 
 # Wave 2
-def uses_available_letters(input, letters_in_hand)
-  input_letters = input.split('').sort
+def uses_available_letters?(test_word, drawn_letters)
+  input_letters = test_word.split('')
   input_letters = input_letters.map {|letter| letter.upcase}
-  puts input_letters
-  letters_in_hand = letters_in_hand.sort
-  if input_letters == letters_in_hand
-    puts true
-  else
-    puts false
+  # puts input_letters
+  
+  input_letters.each do |letter|
+    verify = drawn_letters.include?(letter)
+    if verify == true
+      drawn_letters.delete(letter)
+    else
+      return false
+    end
   end
+  return true
 end
 
-uses_available_letters("ada", ["A","D","A"])
+puts uses_available_letters?("kada", ["A","D","A","C","K"])
