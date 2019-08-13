54e2ee2dfda0932c481eb3aa4a6d5b6e3a0c5195
Julia Kingrey <juliakingrey@Julias-MBP.localdomain>
2019-08-13 15:50:41 -0700

    Added code that turns user input string into array of characters

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -28,20 +28,28 @@
 # array
 # => ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"]
 
-def draw_letters
-  remaining_pool = %w(a a a a a a a a a b b c c d d d d e e e e e e e e e e e e f f g g g h h i i i i i i i i i j k l l l l m m n n n n n n o o o o o o o o p p q r r r r r r s s s s t t t t t t u u u u v v w w x y y z)
-  remaining_pool.map!(&:upcase)
-  user_hand = []
-  
-  10.times do |letter|
-    letter = remaining_pool.sample
-    user_hand.push(letter)
-    remaining_pool.slice!(remaining_pool.index(letter))
-  end 
-  p remaining_pool
-  return user_hand
-  
-end 
+# def draw_letters
+#   remaining_pool = %w(a a a a a a a a a b b c c d d d d e e e e e e e e e e e e f f g g g h h i i i i i i i i i j k l l l l m m n n n n n n o o o o o o o o p p q r r r r r r s s s s t t t t t t u u u u v v w w x y y z)
+#   remaining_pool.map!(&:upcase)
+#   letters_in_hand = []
 
-result = draw_letters
-p result 
+#   10.times do |letter|
+#     letter = remaining_pool.sample
+#     letters_in_hand.push(letter)
+#     remaining_pool.slice!(remaining_pool.index(letter))
+#   end 
+#   p remaining_pool
+#   return letters_in_hand
+
+# end 
+
+# result = draw_letters
+# p result 
+
+# def uses_available_letters(input, letters_in_hand)
+#   input_array = input.chars
+#   p input_array
+# end 
+input = 'Hello'
+input_array = input.chars
+p input_array
