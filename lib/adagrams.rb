1ca9f10dce5e94f0ae9f38bb625c5261ea25b95f
Ga-Young Jin <ga-youngjin@Ga-Youngs-MBP-2.localdomain>
2019-08-13 16:40:31 -0700

    Completed wave-3 for adagrams.rb

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -90,3 +90,46 @@ def uses_available_letters?(input, letters_in_hand)
   end
 end
 
+# wave-3
+def score_word(word)
+  word_as_array = word.upcase.split("") 
+  
+  score_chart = {
+    A: 1,
+    E: 1,
+    I: 1,
+    O: 1,
+    U: 1,
+    L: 1, 
+    N: 1, 
+    R: 1, 
+    S: 1, 
+    T: 1,
+    D: 2, 
+    G: 2,
+    B: 3, 
+    C: 3, 
+    M: 3, 
+    P: 3,
+    F: 4,
+    H: 4,
+    V: 4,
+    W: 4,
+    Y: 4,
+    K: 5, 
+    J: 8,
+    X: 8,
+    Q: 10,
+    Z: 10
+  }
+  
+  # calculate score based on letters in word
+  score = 0
+  word_as_array.each do |char|
+    score += score_chart[char.to_sym]
+  end
+  if word.length > 6
+    score += 8
+  end
+  return score
+end
