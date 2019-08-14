9c7d4fdcd5cbb1504140b45563678fa89b1c5b5b
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-08-14 13:57:10 -0700

    added to uses_available_letters? method to make sure noduplicate letters are used that aren't in the letters_in_hand_array, tested and works.

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -8,9 +8,29 @@ end
 
 ARRAY_OF_LETTERS = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"]
 
+# def duplicate_check (input, letters_in_hand)
+#   dup_input = input
+
+#   dup_input.each do |chars|
+#     letters_in_hand.delete(chars)
+#   end
+#   p letters_in_hand
+# end
+
+
 def uses_available_letters? (input, letters_in_hand)
   input = input.upcase.chars
-  #p input
+  dup_letters_in_hand = letters_in_hand.clone
+  
+  input.each do |chars|
+    if dup_letters_in_hand.include?(chars)
+      dup_letters_in_hand.delete(chars)
+    else 
+      raise ArgumentError, "Cannot use letters not in hand"
+    end
+  end
+  
+  
   check = 0
   input.each do |chars|
     if letters_in_hand.include?(chars)
@@ -19,8 +39,10 @@ def uses_available_letters? (input, letters_in_hand)
       check += 1
     end
   end
+  # duplicate_check(input, letters_in_hand)
   return check == 0
 end
+
 
 def score_word(word)
   letter_points = {
@@ -31,5 +53,8 @@ def score_word(word)
     letter_points[k]
   end
   total_points = word_points.sum
+  if word.length > 6 && word.length < 11
+    total_points += 8
+  end
   return total_points
 end
