943eec5168fd61ef6e19a274b876ed1722daa8fe
Dianna Tingg <dtingg@gmail.com>
2019-08-14 08:19:34 -0700

    fixed spacing problems

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -2,11 +2,8 @@ require "CSV"
 
 # Wave 1
 def draw_letters
-  
-  all_tiles = { 
-    A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9,  J: 1, K: 1, L: 4, M: 2, 
-    N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1 
-  }
+  all_tiles = { A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9,  J: 1, K: 1, L: 4, M: 2, 
+  N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1 }
   
   tile_array = []
   
@@ -39,14 +36,13 @@ end
 def score_word(word)
   
   score_hash = {
-    A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
-    D: 2, G: 2,
-    B: 3, C: 3, M: 3, P: 3,
-    F: 4, H: 4, V: 4, W: 4, Y: 4,
-    K: 5,
-    J: 8, X: 8,
-    Q: 10, Z:10
-  }
+  A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2,
+  B: 3, C: 3, M: 3, P: 3,
+  F: 4, H: 4, V: 4, W: 4, Y: 4,
+  K: 5,
+  J: 8, X: 8,
+  Q: 10, Z: 10 }
   
   score_total = 0
   
@@ -84,17 +80,16 @@ def highest_score_from(words)
   if highest_words.length == 1
     winning_word = highest_words[0]
   else
-    
     highest_words.each do |word|
       if word.length == 10
         winning_word = word
         break
       end
-      
-      if !winning_word
-        winning_word = highest_words.min_by do |word|
-          word.length
-        end
+    end
+    
+    if !winning_word
+      winning_word = highest_words.min_by do |word|
+        word.length
       end
     end
   end
