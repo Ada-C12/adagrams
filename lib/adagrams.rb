f5cc26069eacf954fdbcfd29778f10b2f0b6d8ee
emaust <erikamaust@gmail.com>
2019-08-14 15:36:01 -0700

    Wave 3 completed

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -18,29 +18,38 @@ end
 hand = draw_letters
 p hand
 
-def users_available_letters?(input, letters_in_hand) 
-  #puts letters_in_hand
-  def users_available_letters?(input, letters_in_hand)
-    input.each do |char|
-      if !letters_in_hand.include?(char)
-        puts “this didn’t work”
-        return false
-      end
-      letters_in_hand.delete_at(letters_in_hand.index(char))
+#WAVE 2
+def users_available_letters?(input, letters_in_hand)
+  input.each do |char|
+    if !letters_in_hand.include?(char)
+      puts "This didn't work."
+      return false
     end
-    p letters_in_hand
-    return true
+    letters_in_hand.delete_at(letters_in_hand.index(char))
   end
-  
-  puts “Enter a word.”
-  user_word = gets.chomp.upcase
-  word_array = user_word.chars
-  
-  users_available_letters?(word_array, hand)
-  
-  # def score_word(word)
-  # score_chart = {%w[A E I O U L N R S T] => 1, %w[D G] => 2, %w[B C M P] => 3, %w[F H B W Y] => 4, %w[K] => 5, %w[J X]=> 8, %w[Q Z] => 10}
-  
-  
-  
-  
+  p letters_in_hand
+  return true
+end
+
+puts "Enter a word."
+user_word = gets.chomp.upcase
+word_array = user_word.chars
+
+users_available_letters?(word_array, hand)
+
+#WAVE 3
+def score_word(word)
+  score_chart = {"A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1, "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1, "D" => 2, "G" => 2, "B" => 3, "C" => 3, "M" => 3, "P" => 3, "F" => 4, "H" => 4, "V" => 4, "W" => 4, "Y" => 4, "K" => 5, "J" => 8, "X" => 8, "Q" => 10, "Z" => 10}
+  score = 0
+  if word.length >= 7
+    score = 8
+  end
+  word.each do |char|
+    points = score_chart[char]
+    points = points.to_i
+    score += points
+  end
+  p score
+end
+
+score_word(word_array)
