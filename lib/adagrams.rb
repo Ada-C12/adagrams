69841a73306d3c8766e09a337961297a51b63ca2
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-08-14 13:45:19 -0700

    wave 5 works FINALLY

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,23 +1,23 @@
-require 'csv'
+require "csv"
 
 def draw_letters()
   letter_pool = ["A"] * 9 + ["B"] * 2 + ["C"] * 2 + ["D"] * 4 + ["E"] * 12 + ["F"] * 2 + ["G"] * 3 + ["H"] * 2 + ["I"] * 9 + ["J"] + ["K"] + ["L"] * 4 + ["M"] * 2 + ["N"] * 6 + ["O"] * 8 + ["P"] * 2 + ["Q"] + ["R"] * 6 + ["S"] * 4 + ["T"] * 6 + ["U"] * 4 + ["V"] * 2 + ["W"] * 2 + ["X"] + ["Y"] * 2 + ["Z"]
   hand = []
-  
+
   # Shuffling once resulted in too many duplicates.
   # The program shuffles multiple times.
   10.times do
     letter_pool.shuffle!
     hand << letter_pool.pop
   end
-  
+
   return hand
 end
 
 def uses_available_letters?(input, letters_in_hand)
   split = input.upcase.split("")
   copy = letters_in_hand.dup
-  
+
   split.each do |letter|
     if copy.include? (letter)
       copy.delete_at(copy.index(letter))
@@ -25,9 +25,8 @@ def uses_available_letters?(input, letters_in_hand)
       return false
     end
   end
-  
+
   return true
-  
 end
 
 def score_word(word)
@@ -51,25 +50,25 @@ def score_word(word)
       points += 10
     end
   end
-  
+
   if word_arr.length >= 7
     points += 8
   end
-  
+
   return points
 end
 
 def highest_score_from(words)
   word_score = []
-  
+
   words.each do |word|
     word_score << { word: word, score: score_word(word) }
   end
-  
+
   max = word_score.max_by { |element| element[:score] }[:score]
   winners = word_score.select { |element| element[:score] == max }
   ten_char = winners.select { |element| element[:word].length == 10 }
-  
+
   if ten_char.length > 0
     return ten_char.first
   else
@@ -78,13 +77,12 @@ def highest_score_from(words)
   end
 end
 
-#Only works if you know the absolute file path to the dictionary on your computer.
-# def is_in_english_dict?(input)
-#   dictionary = CSV.read("../assets/dictionary-english.csv")
+def is_in_english_dict?(input)
+  dictionary = CSV.read("assets/dictionary-english.csv")
 
-#   if dictionary.include? [input]
-#     return true
-#   else
-#     return false
-#   end
-# end
+  if dictionary.include? [input]
+    return true
+  else
+    return false
+  end
+end
