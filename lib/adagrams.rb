21f849ce430a3b67a7750542b336ceab9144d0a7
Mira <calopter@gmail.com>
2019-08-14 15:35:17 -0700

    refactoring for clarity

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,5 +1,6 @@
-LETTER_POOL = {A: 9, N: 6, B: 2, O: 8, C: 2, P: 2, D: 4, Q: 1, E: 12, R: 6, F: 2, S: 4, G: 3,	T: 6, H: 2,	U: 4, I: 9,	V: 2, J: 1,	W: 2, K: 1,	X: 1, L: 4,	Y: 2, M: 2, Z: 1}
-
+LETTER_POOL = { A: 9, N: 6, B: 2, O: 8, C: 2, P: 2, D: 4, Q: 1, E: 12, 
+                R: 6, F: 2, S: 4, G: 3, T: 6, H: 2, U: 4, I: 9, V: 2, 
+                J: 1, W: 2, K: 1, X: 1, L: 4, Y: 2, M: 2, Z: 1 }
 
 def draw_letters 
   letters = LETTER_POOL.map do |letter, count|
@@ -9,21 +10,23 @@ def draw_letters
   return letters.flatten.sample(10)
 end
 
-# this method may change strings in letters_in_hand 
 def uses_available_letters?(input, letters_in_hand)
-  letters_in_hand = letters_in_hand.clone
-  input.each_char do |c|
-    index = letters_in_hand.find_index(c)
+  letters = letters_in_hand.clone
+
+  input.upcase.each_char do |c|
+    index = letters.find_index(c)
+
     if index.nil?
       return false
     else
-      letters_in_hand.delete_at(index)
+      letters.delete_at(index)
     end 
   end
-return true
+
+  return true
 end
 
-SCORE_CHART = [
+scores = [
   {
     letters: %w(A E I O U L N R S T),
     value: 1
@@ -54,23 +57,25 @@ SCORE_CHART = [
   }
 ]
 
-# refactor!
-def score_word(word)
-  total_points = 0
-  word.upcase.each_char do |c|
-    SCORE_CHART.each do |score|
-      if score[:letters].include?(c)
-        total_points += score[:value]
-      end
-    end
+# make scores into a global hash
+SCORE_CHART = {}
+scores.each do |score| 
+  score[:letters].each do |letter| 
+    SCORE_CHART[letter] = score[:value]
   end
-  if word.length > 6
-    total_points += 8
-  end
-  return total_points
 end
 
-# wave 4
+def score_word(word)
+  total_points = 0
+
+  word.upcase.each_char do |c|
+    total_points += SCORE_CHART[c]
+  end
+
+  total_points += 8 if word.length >= 7
+
+  return total_points
+end
 
 def break_tie(word_1, word_2)
   length_1 = word_1[:word].length
@@ -84,6 +89,7 @@ end
 
 def highest_score_from(words)
   winning_word = { word: nil, score: 0}
+
   words.each do |word|
     score = score_word(word) #the score of current word
 
