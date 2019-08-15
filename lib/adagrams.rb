ea9b0fb4a111c1d0f7186b3ea018817a3d6563f0
eaball35 <eaball35@gmail.com>
2019-08-14 17:32:40 -0700

    Finished wave 4

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,59 +1,57 @@
-# # Wave 1
-# def draw_letters
-#   letter_pool = {
-#     "9" => ["A", "I"], 
-#     "4" => ["D", "L", "S", "U"], 
-#     "2" => ["B", "C", "F", "H", "M", "P", "V", "W", "Y"],  
-#     "12" => ["E"],  
-#     "3" => ["G",],
-#     "1" => ["J", "K", "Q", "X", "Z"],
-#     "6" => ["N", "R", "T"],
-#     "8" => ["O"],
-#   }
+# -  Wave 1 -
+def draw_letters
+  letter_pool = {
+    "9" => ["A", "I"], 
+    "4" => ["D", "L", "S", "U"], 
+    "2" => ["B", "C", "F", "H", "M", "P", "V", "W", "Y"],  
+    "12" => ["E"],  
+    "3" => ["G",],
+    "1" => ["J", "K", "Q", "X", "Z"],
+    "6" => ["N", "R", "T"],
+    "8" => ["O"],
+  }
   
-#   letters = []
+  letters = []
   
-#   letter_pool.each do |key, value|
-#     key_i = key.to_i
-#     key_i.times do
-#       value.each do |string|
-#         letters << string
-#       end
-#     end
-#   end
+  letter_pool.each do |key, value|
+    key_i = key.to_i
+    key_i.times do
+      value.each do |string|
+        letters << string
+      end
+    end
+  end
   
-#   generate = []
-#   10.times do
-#     generate << letters.shuffle.last
-#     letters.pop
-#   end
-#   return generate
-# end
+  generate = []
+  10.times do
+    generate << letters.shuffle.last
+    letters.pop
+  end
+  return generate
+end
 
-# # require 'pry'
 
-# # Wave 2
-# def uses_available_letters?(test_word, drawn_letters)
-#   drawn_letters_2 = drawn_letters.dup
-#   input_letters = test_word.split('')
-#   input_letters = input_letters.map {|letter| letter.upcase}
-#   # binding.pry
-#   input_letters.each do |letter|
-#     index = drawn_letters_2.index(letter)
-#     if index != nil
-#       drawn_letters_2.delete_at(index)
-#     else
-#       return false
-#     end
-#   end
-#   return true
-# end
+# -  Wave 2 - 
+def uses_available_letters?(test_word, drawn_letters)
+  drawn_letters_2 = drawn_letters.dup
+  input_letters = test_word.split('')
+  input_letters = input_letters.map {|letter| letter.upcase}
+  input_letters.each do |letter|
+    index = drawn_letters_2.index(letter)
+    if index != nil
+      drawn_letters_2.delete_at(index)
+    else
+      return false
+    end
+  end
+  return true
+end
 
 
 # # puts uses_available_letters?("kada", ["A","D","A","C","K"])
 
 
-# Wave 3
+# -  Wave 3 - 
 def score_word(word)
   score_chart = {
     "1" => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
@@ -87,27 +85,29 @@ def score_word(word)
   return sum
 end
 
-# # puts score_word("AEIO")
+
   
 # Wave 4
 # require 'pry'
 
 def highest_score_from(words)
-  scores = Hash.new
+  scores = {}
   words.each do |word|
     score = score_word(word)
     # binding.pry
     scores[word] = score
   end
   
-  
   max = scores.values.max
 
-  winners = Array.new
-  winner = Hash.new
-  scores.map do |key,value|
-    if value == max
-      winner = {"#{key}": max}
+  winners = []
+  winner = {}
+  scores.each do |word,score|
+    if score == max
+      winner = {
+        word: "#{word}",
+        score: max
+      }
       winners << winner
     end
   end 
@@ -115,26 +115,36 @@ def highest_score_from(words)
   if winners.length > 1
     min = 11
     win_word = nil
-    winners.map do |key, value|
-      if key.length == 10
-        finalwinner = {"#{key}": value}
-        return finalwinner
-      elsif key.length < min
-        min = key.length
-        win_word = key
+    win_word_score = nil
+    
+    winners.each do |word_hash|
+      if word_hash[:word].length == 10
+        return {
+          word:"#{word_hash[:word]}",
+          score: word_hash[:score]
+        }
+      elsif word_hash[:word].length < min
+        min = word_hash[:word].length
+        win_word = word_hash[:word]
+        win_word_score = word_hash[:score]
       end
     end
-    finalwinner = {"#{win_word}": min}
-    return finalwinner
-  end
+    
+    return {
+      word: "#{win_word}",
+      score: win_word_score
+    }
 
-  return winners
+  else
+    return winners[0]
+  end
+  
 end
 
-words = ["KADA", "KADA", "KADA"]
-p highest_score_from(words)
-
-
+words =  ['BBBBBB', 'AAAAAAAAAA']
+answer = highest_score_from(words)
+puts answer[:word]
+puts answer[:score]
 
 
 # return winner
