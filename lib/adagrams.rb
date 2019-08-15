2d87c4c87f42ae59a0fdaa05d1b6aa921368c9e9
dora1405 <li.dora05@gmail.com>
2019-08-15 14:03:03 -0700

    Completed Wave 5 and refractored final project

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,6 +1,12 @@
+require 'csv'
+
 # -  Wave 1 -
+
+# Create method called draw_letter to build a hand of 10 letters for the user
 def draw_letters
-  letter_pool = {
+  
+  # Create distribution of letters
+  letters = {
     "9" => ["A", "I"], 
     "4" => ["D", "L", "S", "U"], 
     "2" => ["B", "C", "F", "H", "M", "P", "V", "W", "Y"],  
@@ -10,34 +16,45 @@ def draw_letters
     "6" => ["N", "R", "T"],
     "8" => ["O"],
   }
+  # Create empty array to hold multiples of the same letters
+  letter_pool = []
   
-  letters = []
-  
-  letter_pool.each do |key, value|
+  # Get multiples of the same letters by iterating a single letter times the hash key's number
+  letters.each do |key, value|
     key_i = key.to_i
     key_i.times do
       value.each do |string|
-        letters << string
+        letter_pool << string
       end
     end
   end
   
-  generate = []
+  # Generate user's hand containing 10 random letters from the pool of letters
+  generate_hand = []
   10.times do
-    generate << letters.shuffle.last
-    letters.pop
+    generate_hand << letter_pool.shuffle.last
+    letter_pool.pop
   end
-  return generate
+  return generate_hand
 end
 
 
 # -  Wave 2 - 
+
+# Create method uses_available_letter?
+# Method checks if the letters in the user's word matches the collection of drawn letters
 def uses_available_letters?(test_word, drawn_letters)
+  
+  # Create duplicate of drawn_letters to ensure data in this array does not change
   drawn_letters_2 = drawn_letters.dup
+  # Take users's word and identify each character in that word
   input_letters = test_word.split('')
   input_letters = input_letters.map {|letter| letter.upcase}
   input_letters.each do |letter|
+    # Verify user's word character against collection of drawn letters
     index = drawn_letters_2.index(letter)
+    # If user's word character exist in the collection of drawn letters,
+    # delete character from collection of drawn letters to account for repeats of same letter
     if index != nil
       drawn_letters_2.delete_at(index)
     else
@@ -48,10 +65,9 @@ def uses_available_letters?(test_word, drawn_letters)
 end
 
 
-# # puts uses_available_letters?("kada", ["A","D","A","C","K"])
-
-
 # -  Wave 3 - 
+
+# Create method score_word to return the score of a given word as defined by game's point value
 def score_word(word)
   score_chart = {
     "1" => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
@@ -68,17 +84,19 @@ def score_word(word)
   
   sum = 0
   points = 0
+  # Get value/point of each letter and add it all together in 'sum'
   split_word.each do |letter|
     score_chart.each do | key, value|
       if value.include?(letter)
         points = key.to_i
       end
     end
-  
+    
     sum += points
-  
+    
   end
   
+  # If length o fthe word is 7, 8, 9 or 10, then the word gets an additional 8 points
   if split_word.length > 6
     sum += 8
   end
@@ -86,20 +104,24 @@ def score_word(word)
 end
 
 
-  
-# Wave 4
-# require 'pry'
 
+# Wave 4
+
+# Create method called highest_score_from
+# To find the highest scoring word
 def highest_score_from(words)
+  # Create hash named scores to hold word as the key and score of that word as the value
   scores = {}
+  # Find the score of each word and matched to that word key in the hash
   words.each do |word|
     score = score_word(word)
-    # binding.pry
     scores[word] = score
   end
   
+  # Find the highest score
   max = scores.values.max
-
+  
+  # Multiple words have the same max score - aka, a tie
   winners = []
   winner = {}
   scores.each do |word,score|
@@ -111,42 +133,42 @@ def highest_score_from(words)
       winners << winner
     end
   end 
-
+  
+  # Find the tie-breaker
   if winners.length > 1
     min = 11
-    win_word = nil
-    win_word_score = nil
+    tie_word = nil
+    tie_word_score = nil
     
     winners.each do |word_hash|
+      # If one of the tie-breaker words is 10 letters long, then the word with 10 letters win
       if word_hash[:word].length == 10
         return {
           word:"#{word_hash[:word]}",
           score: word_hash[:score]
         }
+        # Else the word with the least letters win the tie-breaker
       elsif word_hash[:word].length < min
         min = word_hash[:word].length
-        win_word = word_hash[:word]
-        win_word_score = word_hash[:score]
+        tie_word = word_hash[:word]
+        tie_word_score = word_hash[:score]
       end
     end
     
     return {
-      word: "#{win_word}",
-      score: win_word_score
+      word: tie_word,
+      score: tie_word_score
     }
-
+    
+    # Return winner if there is no tie-breaker scenario
   else
     return winners[0]
   end
-  
 end
 
-words =  ['BBBBBB', 'AAAAAAAAAA']
-answer = highest_score_from(words)
-puts answer[:word]
-puts answer[:score]
-
-
-# return winner
-
-# end
+# Create method is_in_english_dict?
+# Verify player's word is valid against the English dictionary
+def is_in_english_dict?(input)
+  dictionary = CSV.read('assets/dictionary-english.csv')
+  return dictionary.include?(input)
+end
