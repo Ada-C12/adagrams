0c11823780366c8625d8ffc9470e2bd250e1add0
Daniela Sanchez <dnsanche@Danielas-MBP.localdomain>
2019-08-14 17:34:14 -0700

    Added dictionary, deleted comments, updated key, values names

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 def draw_letters 
   letter_pool = ("a" * 10 + "b" * 2 + "c" * 2 + "d" * 4 + "e" * 12 + "f" * 2 + "g" * 3 + "h" * 2 + "i" * 9 + "j" + "k" + "l" * 4 + "m" * 2 + "n" * 6 + "o" * 8 + "p" * 2 + "q" + "r" * 6 + "s" * 4 + "t" * 6 + "u" * 4 + "v" * 2 + "w" * 2 + "x" + "y" * 2 + "z").chars
   letter_hand = []
@@ -32,9 +34,9 @@ def score_word(word)
   score = 0
   word_array = word.chars
   word_array.each do |letter| 
-    score_hash.each do |key, value|
-      if key.include?(letter.upcase) 
-        score += value
+    score_hash.each do |letters_string, points|
+      if letters_string.include?(letter.upcase) 
+        score += points
       end
     end 
   end 
@@ -45,12 +47,10 @@ def score_word(word)
 end 
 
 def highest_score_from(words)
-# all words scores
   scores =  words.map do |word|
     score_word(word)
   end   
 
-# words with max score
   max_score = scores.max 
   max_score_index = scores.each_index.select{|i| scores[i] == max_score}
 
@@ -68,21 +68,30 @@ def highest_score_from(words)
       best_score_words << words[i]   
       best_score_words_length << words[i].length
     end
-    if best_score_words_length.include?(10)  #both words have same max score and one word has 10 letters
+    if best_score_words_length.include?(10)  
     winning_index = best_score_words_length.index(10)
     return winning_word = {
       word: best_score_words[winning_index],
       score: max_score
     } 
-    # if tie scores, but none have length 10
     else 
-          return winning_word = { 
-            word: best_score_words.min { |a, b| a.length <=> b.length },
-            score: max_score
-          } 
-      end 
+      return winning_word = 
+      { 
+        word: best_score_words.min { |a, b| a.length <=> b.length },
+        score: max_score
+      } 
+  end 
   end
 end
 
+def is_in_english_dict?(user_input_word)
+  CSV.foreach("/Users/dnsanche/adagrams/assets/dictionary-english.csv") do |row|
+    if row[0].to_s == user_input_word
+      return true
+    end
+  end
+  return false
+end
 
+#problem. We were deleting each letter that the user was getting, but with the original function, it wasn't deleting only one letter, it was deleting all letters from the bank that matched.
 
